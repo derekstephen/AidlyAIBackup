fc0da3acac9dfc28fb87160da4b188f97616ffa3
Derek <7885194+derekstephen@users.noreply.github.com>
2020-03-08 04:51:11 -0400

    Rename File, Clean Starting Code for Branches

--- code.py
+++ code.py
@@ -8,23 +8,22 @@ Created on Sat Mar  7 02:55:49 2020
 
 # Load Libraries & Dependencies
 from nltk.corpus import stopwords
-import numpy as np
 import pandas as pd
+import numpy as np
+import string
+import nltk
+import re
 
 # First time download stop words
-import nltk
 nltk.download('stopwords')
-
-# Import data
-df = pd.read_csv("./Data/MISSION.csv")
-
-# Convert to all lowercase
-df = df.apply(lambda x: x.astype(str).str.lower())
-
-# Remove unnecessary Columns
-df = df[['EIN', 'NAME', 'F9_03_PZ_MISSION' ]]
 
 # Load Stop Words
 stop_words = stopwords.words('english')
 
-# Filter Stop Words from Mission
+# Import data
+df = pd.read_csv("./Data/MISSION.csv")
+
+# Remove unnecessary Columns
+df = df[['EIN', 'NAME', 'F9_03_PZ_MISSION']]
+
+df = df.rename(columns={'F9_03_PZ_MISSION': 'MISSION'})
