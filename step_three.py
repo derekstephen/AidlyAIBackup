39340c6804b81553d21553520fad8d7f0096a36c
Derek <7885194+derekstephen@users.noreply.github.com>
2020-03-13 04:01:51 -0400

    Create step_three.py

---
+++ step_three.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 04:01:39 2020
+
+@author: Derek
+"""
+
+# Load Libraries & Dependencies
+from nltk.corpus import stopwords
+from nltk import ngrams, FreqDist
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+import pandas as pd
+import numpy as np
+import string
+import nltk
+import re
+
+# PREPARE DATA CODE
+
+# First time download stop words
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+# Load Stop Words
+stop_words = stopwords.words('english')
+
+# Import data
+df = pd.read_csv("./Data/MISSION.csv")
