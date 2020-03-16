c9f33c80eb122de5eca9353104efab2d62f4d9f9
Derek <7885194+derekstephen@users.noreply.github.com>
2020-03-16 02:31:10 -0400

    Update prepare_data.py
    Finalized Prepare Data Code

--- prepare_data.py
+++ prepare_data.py
@@ -7,24 +7,19 @@ Created on Thu Mar 12 17:37:37 2020
 
 # Load Libraries & Dependencies
 from nltk.corpus import stopwords
-from nltk import ngrams, FreqDist
-from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 import pandas as pd
-import numpy as np
-import string
 import nltk
-import re
 
+
+def prep_text(mission):
+    sentences = nltk.sent_tokenize(mission)
+    sentences = [nltk.word_tokenize(sent) for sent in sentences]
+    return sentences
 
 
 # First time download stop words
 nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
 
 # Load Stop Words
 stop_words = stopwords.words('english')
@@ -32,33 +27,12 @@ stop_words = stopwords.words('english')
 # Import data
 df = pd.read_csv("./Data/MISSION.csv")
 
-"""
+# Split Mission to Sentences and then Words
+df_missions = df["F9_03_PZ_MISSION"].apply(lambda x: prep_text(str(x).lower()))
+df["MISSION"] = df_missions
 
-text = df.iloc[0]
+# Flatten Separated Words to one List
+df["WORDS"] = df["MISSION"].apply(lambda column: [y for x in column for y in x])
 
-mission = text["F9_03_PZ_MISSION"].lower()
-
-sent_tokenized = nltk.sent_tokenize(mission)
-
-word_tokenized = [nltk.word_tokenize(sent) for sent in sent_tokenized]
-
-print(word_tokenized)
-
-
-"""
-
-
-# Make mission lowercase & Remove Stop Words
-df_missions = df["F9_03_PZ_MISSION"].apply(lambda x: item for item in (nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(str(x).lower())) if item not in stop_words)
-df["F9_03_PZ_MISSION"] = df_missions
-
-# Print Example Mission Statement
-print(df.iloc[0])
-
-pos_tags = [nltk.pos_tag(sent) for sent in df.iloc[0]["F9_03_PZ_MISSION"]]
-
-
-
-
-
-
+# Remove Stop Words
+df["WORDS"] = df["WORDS"].apply(lambda x: [item for item in x if item not in stop_words])
