19127e2f984f75842fc00a1c92f0b663dbea751a
Derek <7885194+derekstephen@users.noreply.github.com>
2020-03-16 03:48:29 -0400

    Update step_one.py
    Remove current unnecessary imports, nltk downloads and fix comments format

--- step_one.py
+++ step_one.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sat Mar  7 02:55:49 2020
@@ -7,22 +6,18 @@ Created on Sat Mar  7 02:55:49 2020
 
 # Load Libraries & Dependencies
 from nltk.corpus import stopwords
-from nltk import ngrams, FreqDist
-from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import *
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 import pandas as pd
-import numpy as np
-import string
 import nltk
-import re
 
 # PREPARE DATA CODE
 
 
 def prep_text(mission):
+    """Preps mission statement by tokenizing sentences and words."""
     sentences = nltk.sent_tokenize(mission)
     sentences = [nltk.word_tokenize(sent) for sent in sentences]
     return sentences
@@ -30,8 +25,6 @@ def prep_text(mission):
 
 # First time download stop words
 nltk.download('stopwords')
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
 
 # Load Stop Words
 stop_words = stopwords.words('english')
@@ -51,6 +44,8 @@ df["WORDS"] = df["WORDS"].apply(lambda x: [item for item in x if item not in sto
 
 # END PREPARE DATA CODE
 
-# Start Step One Code
+# START STEP ONE CODE
 
-# End Step One Code
+
+
+# END STEP ONE CODE
