741ff22d80cfc0dfa9d2b1be11c4e1a2bdaab2df
Derek <7885194+derekstephen@users.noreply.github.com>
2020-03-16 05:13:24 -0400

    Update step_one.py
    Add Lemmatization Code. Inquire issue of setting POS for Lemmatization as V instead of per word.

--- step_one.py
+++ step_one.py
@@ -6,7 +6,7 @@ Created on Sat Mar  7 02:55:49 2020
 
 # Load Libraries & Dependencies
 from nltk.corpus import stopwords
-from nltk.stem import snowball
+from nltk.stem import snowball, WordNetLemmatizer
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 
@@ -32,23 +32,37 @@ stop_words = stopwords.words('english')
 # Import data
 df = pd.read_csv("./Data/MISSION.csv")
 
+# Separate Mission to Sentences
+df["MISSION"] = df["F9_03_PZ_MISSION"].apply(lambda x: nltk.sent_tokenize(str(x).lower()))
+
 # Split Mission to Sentences and then Words
-df["MISSION"] = df["F9_03_PZ_MISSION"].apply(lambda x: prep_text(str(x).lower()))
+df["WORD"] = df["F9_03_PZ_MISSION"].apply(lambda x: prep_text(str(x).lower()))
 
 # Flatten Separated Words to one List
-df["WORDS"] = df["MISSION"].apply(lambda column: [y for x in column for y in x])
+df["WORD"] = df["WORD"].apply(lambda column: [y for x in column for y in x])
 
 # Remove Stop Words
-df["WORDS"] = df["WORDS"].apply(lambda x: [item for item in x if item not in stop_words])
+df["WORD"] = df["WORD"].apply(lambda x: [item for item in x if item not in stop_words])
 
 # END PREPARE DATA CODE
 
 # START STEP ONE CODE
 
+# First time download wordnet
+nltk.download('wordnet')
+
 # Create Porter Stemmer
 stemmer = snowball.SnowballStemmer('english')
 
 # Stem mission statements
-df["STEMMER"] = df["WORDS"].apply(lambda x: [stemmer.stem(word) for word in x])
+df["STEMMER"] = df["WORD"].apply(lambda x: [stemmer.stem(word) for word in x])
+
+# Get POS for each word to use in Lemmatizer
+
+# Create WordNet Lemmatization
+wordnet_lemmatizer = WordNetLemmatizer()
+
+# Lemmatization of Words
+df["LEMMATIZATION"] = df["WORD"].apply(lambda x: [wordnet_lemmatizer.lemmatize(word, pos="v") for word in x])
 
 # END STEP ONE CODE
