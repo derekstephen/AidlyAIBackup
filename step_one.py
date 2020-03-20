38985c66d0da6dad7eed052c99dd5ab987d13692
Derek <7885194+derekstephen@users.noreply.github.com>
2020-03-20 19:57:07 -0400

    Update step_one.py
    Still doesn't work, but it's a good start

--- step_one.py
+++ step_one.py
@@ -5,7 +5,7 @@ Created on Sat Mar  7 02:55:49 2020
 """
 
 # Load Libraries & Dependencies
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, wordnet
 from nltk.stem import snowball, WordNetLemmatizer
 
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -24,7 +24,7 @@ def prep_text(mission):
 
 
 # First time download stop words
-nltk.download('stopwords')
+#nltk.download('stopwords')
 
 # Load Stop Words
 stop_words = stopwords.words('english')
@@ -38,7 +38,7 @@ df["MISSION"] = df["F9_03_PZ_MISSION"].apply(lambda x: nltk.sent_tokenize(str(x)
 # Split Mission to Sentences and then Words
 df["WORD"] = df["F9_03_PZ_MISSION"].apply(lambda x: prep_text(str(x).lower()))
 
-# Flatten Separated Words to one List
+# Flatten Separated Words to one list
 df["WORD"] = df["WORD"].apply(lambda column: [y for x in column for y in x])
 
 # Remove Stop Words
@@ -48,8 +48,23 @@ df["WORD"] = df["WORD"].apply(lambda x: [item for item in x if item not in stop_
 
 # START STEP ONE CODE
 
+
+def get_wordnet_pos(treebank_tag):
+    """Convert POS String to POS for Lemmatizer."""
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
+    
+
 # First time download wordnet
-nltk.download('wordnet')
+#nltk.download('wordnet')
 
 # Create Porter Stemmer
 stemmer = snowball.SnowballStemmer('english')
@@ -58,11 +73,22 @@ stemmer = snowball.SnowballStemmer('english')
 df["STEMMER"] = df["WORD"].apply(lambda x: [stemmer.stem(word) for word in x])
 
 # Get POS for each word to use in Lemmatizer
+df["POS"] = df["WORD"].apply(lambda x: [nltk.pos_tag(x)])
 
 # Create WordNet Lemmatization
 wordnet_lemmatizer = WordNetLemmatizer()
 
+#Flatten POS to one list
+df["POS"] = df["POS"].apply(lambda column: [y for x in column for y in x])
+
 # Lemmatization of Words
-df["LEMMATIZATION"] = df["WORD"].apply(lambda x: [wordnet_lemmatizer.lemmatize(word, pos="v") for word in x])
+df["LEMMATIZATION"] = df["POS"].apply(lambda x: [wordnet_lemmatizer.lemmatize(pair[0], pos=get_wordnet_pos(pair[1])) if get_wordnet_pos(pair[1]) != '' else wordnet_lemmatizer.lemmatize(pair[0]) for sent in x for pair in sent])
+
+# Lemmatization of Words
+#df["LEMMATIZATION"] = df["POS"].apply(lambda x: [wordnet_lemmatizer.lemmatize(pair[0], pos=get_wordnet_pos(pair[1])) for sent in x for pair in sent])
 
 # END STEP ONE CODE
+
+# Separate Data to view easily
+
+df_imp = df[["EIN", "NAME", "F9_03_PZ_MISSION", "MISSION", "WORD", "POS", "STEMMER", "LEMMATIZATION"]]
